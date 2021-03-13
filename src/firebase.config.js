27377b79c51050cf44f712c1ecf3f2a8160533e2
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-03-13 14:38:31 +0600

    finish

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCN5FfkVCnSruAq12HQuHjWKL7SbSgy3B4",
+    authDomain: "ema-john-simple-4e3c7.firebaseapp.com",
+    projectId: "ema-john-simple-4e3c7",
+    storageBucket: "ema-john-simple-4e3c7.appspot.com",
+    messagingSenderId: "256266028144",
+    appId: "1:256266028144:web:7c4f8ebd4d3c9bf876a0b5"
+  };
+  export default firebaseConfig;
